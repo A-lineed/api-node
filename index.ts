746095198d1336d5bccbd66fee05a5e75688f38a
A-lineed <alineedvania2018@gmail.com>
2022-09-06 08:00:52 -0300

    Construindo a primeira rota

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Criando API com Node e TS")
+import express, {Request, response, Response} from "express";
+
+const server = express();
+
+server.get('/', (request: Request, response: Response) => {
+    return response.json({mensagem: 'Bem vindo a nossa DIO API http://localhost:5000/'})
+
+})
+
+server.listen(5000, () => {
+    console.log('Servidor on na porta 5000')
+})
